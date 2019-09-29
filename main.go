771938de5ab366c3153acb7854d93e6a7c4649a7
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-30 01:01:20 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/IndBrony/lettercounter"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type response struct {
+	Vocals     int `json:"vocals"`
+	Consonants int `json:"consonants"`
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+
+	text := request.Form.Get("text")
+	vocals, consonants := lettercounter.NumberOfVocalsAndConsonants(text)
+
+	response := response{
+		Vocals:     vocals,
+		Consonants: consonants,
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(writer).Encode(response)
+	if err != nil {
+		log.Printf("Error writing json: %v", err)
+		http.Error(writer, "can't write json", http.StatusInternalServerError)
+		return
+	}
+}
